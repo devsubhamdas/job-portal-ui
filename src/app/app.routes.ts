1cd83797afd0981f0aecb216e635fd1c0c06f5a4
Subham-Das-98 <das198subham@gmail.com>
2026-05-30 14:01:34 +0530

    router setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/job-search/job-search').then((m) => m.JobSearch),
+  },
+  {
+    path: 'applications',
+    loadComponent: () => import('./pages/applications/applications').then((m) => m.Applications),
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./pages/admin/admin').then((m) => m.Admin),
+  },
+];
---
+++ src/app/pages/job-search/job-search.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-search',
+  imports: [],
+  templateUrl: './job-search.html',
+  styleUrl: './job-search.css',
+})
+export class JobSearch {}
