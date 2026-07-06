014dc49776eaa627183a203fb47e0142d58aaf2e
Subham-Das-98 <das198subham@gmail.com>
2026-07-06 21:24:12 +0530

    chore:remove unnecessary code

--- src/app/guards/admin/admin-guard.ts
+++ src/app/guards/admin/admin-guard.ts
@@ -1,28 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { filter, map, take } from 'rxjs';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
-  // return toObservable(auth.authLoading).pipe(
-  //   filter((loading) => !loading),
-  //   take(1),
-  //   map(() => {
-  //     const user = auth.user();
-
-  //     if (!user) {
-  //       return router.createUrlTree(['/']);
-  //     }
-
-  //     return user.role === 'ADMIN' ? true : router.createUrlTree(['/']);
-  //   }),
-  // );
   const user = auth.user();
-
   if (!user) {
     return router.createUrlTree(['/']);
   }
--- src/app/guards/guest/guest-guard.ts
+++ src/app/guards/guest/guest-guard.ts
@@ -1,20 +1,10 @@
 import { inject } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
 import { CanActivateFn, Router } from '@angular/router';
-import { filter, map, take } from 'rxjs';
 import { AuthService } from '../../services/auth/auth.service';
 
 export const guestGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
-
-  // return toObservable(auth.authLoading).pipe(
-  //   filter((loading) => !loading),
-  //   take(1),
-  //   map(() => {
-  //     return auth.user() ? router.createUrlTree(['/']) : true;
-  //   }),
-  // );
 
   return auth.user() ? router.createUrlTree(['/']) : true;
 };
