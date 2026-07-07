b3ba37a3098eb7c9bb145489c3215981ba80e69f
Subham-Das-98 <das198subham@gmail.com>
2026-07-07 12:30:01 +0530

    configure ssr allowed-host/setup page specific render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Server },
+  { path: 'applications', renderMode: RenderMode.Client },
+  { path: 'admin', renderMode: RenderMode.Client },
+  { path: 'signup', renderMode: RenderMode.Client },
   {
     path: '**',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
 ];
